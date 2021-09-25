4814d113e3a834637f2be24ed218e0ec41be44af
anton2009danilov <anton2009danilov@yandex.ru>
2021-09-25 20:00:30 +0300

    fixed clickable invisible burger-menu

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 "use strict";
 
-const headerBurgerBtn = document.getElementById("header-burger-btn");
-const headerBurgerList = document.getElementById("header-burger-list")
+window.onload = () => {
 
-headerBurgerBtn.addEventListener("click", (e) => {
-	if (!headerBurgerList.style.opacity || headerBurgerList.style.opacity === "0") {
-		headerBurgerList.style.opacity = "1";
-	}
-	else {
-		headerBurgerList.style.opacity = "0";
-	}
-});
+	const headerBurgerBtn = document.getElementById("header-burger-btn");
+	const headerBurgerList = document.getElementById("header-burger-list")
+
+	headerBurgerBtn.addEventListener("click", (e) => {
+		if (!headerBurgerList.style.display || headerBurgerList.style.display === "none") {
+			headerBurgerList.style.display = "flex";
+		}
+		else {
+			headerBurgerList.style.display = "none";
+		}
+	});
+
+};
