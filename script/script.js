b687fa5588c6df62e34df8d4009b7353a48fe779
anton2009danilov <anton2009danilov@yandex.ru>
2021-09-30 17:21:42 +0300

    add floating placeholder for user-comment textarea

--- script/script.js
+++ script/script.js
@@ -11,4 +11,17 @@ window.onload = () => {
 			emailPlaceholder.classList.remove("placeholder--active")
 		}
 	})
+
+	const commentTextarea = document.getElementById("comment");
+	const commentPlaceholder = document.getElementById("comment-placeholder");
+
+	commentTextarea.addEventListener("change", (e) => {
+		console.log(commentTextarea.value === "");
+		if (commentTextarea.value !== "") {
+			commentPlaceholder.classList.add("placeholder--active");
+		} else {
+			commentPlaceholder.classList.remove("placeholder--active")
+		}
+	})
+
 };
