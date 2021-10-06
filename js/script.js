e30ea36d89d63f15245a8bd9d81c3c30692314c7
anton2009danilov <anton2009danilov@yandex.ru>
2021-10-06 20:48:59 +0300

    fix clickable hidden burger-menu

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const headerBurgerBtn = document.getElementById("header-burger-btn");
 const headerBurgerList = document.getElementById("header-burger-list")
 
 headerBurgerBtn.addEventListener("click", (e) => {
-	if (!headerBurgerList.style.opacity || headerBurgerList.style.opacity === "0") {
-		headerBurgerList.style.opacity = "1";
+	if (!headerBurgerList.style.display || headerBurgerList.style.display === "none") {
+		headerBurgerList.style.display = "flex";
 	}
 	else {
-		headerBurgerList.style.opacity = "0";
+		headerBurgerList.style.display = "none";
 	}
 });
